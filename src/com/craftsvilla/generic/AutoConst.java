4b666e55bfd77796e2d5ef8f97c9fa3a2ae74c4b
SharonThomas131 <thomassharon131@gmail.com>
2019-02-06 00:33:38 -0600

    Added a comment to autoConst

--- src/com/craftsvilla/generic/AutoConst.java
+++ src/com/craftsvilla/generic/AutoConst.java
@@ -1,5 +1,6 @@
 package com.craftsvilla.generic;
 
+//Interface which stores all the constant values of the project
 public interface AutoConst {
 	String CHROME_KEY = "webdriver.chrome.driver";    //industry standard way, final var is capitals
 	String CHROME_VALUE = ".\\Drivers\\chromedriver.exe";
